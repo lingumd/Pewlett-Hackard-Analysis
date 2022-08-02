b7a43a87c8d5b3ed0330e4b7f4db515e97bedf9d
Deanna Lingum <lingumd@gmail.com>
2022-08-02 05:52:21 -0400

    Uploading updated queries.

--- Employee_Challenge.sql
+++ Employee_Challenge.sql
@@ -1,10 +1,70 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Get information on retiring employees and their titles
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+
+-- Get only latest titles of retiring employees
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+
+
+-- Get amount of retiring employees in each job title
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+
+-- Mentorship Program
+-- Get employees eligible to participate in a mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+de.from_date, de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT COUNT(title), title
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT SUM(count)
+FROM retiring_titles;
+
+SELECT e.emp_no, t.title
+INTO not_retiring_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE e.emp_no NOT IN (SELECT emp_no FROM retirement_titles)
+AND t.to_date = '9999-01-01';
+
+SELECT COUNT(title), title
+FROM not_retiring_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
